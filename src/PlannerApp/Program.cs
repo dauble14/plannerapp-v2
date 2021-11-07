fc72f2e1fedeae0a35f95465ee4d4ae450c9537d
dauble14 <kuba08052001@gmail.com>
2021-11-07 14:23:46 +0100

    authorization added

--- src/PlannerApp/Program.cs
+++ src/PlannerApp/Program.cs
@@ -1,3 +1,5 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,9 +20,19 @@ namespace PlannerApp
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient("PlannerApp.Api", client=> 
+            {
+                client.BaseAddress = new Uri("https://https://plannerapp-api.azurewebsites.net/");
+            }).AddHttpMessageHandler<AuthorizationMessageHandler>();
 
+            builder.Services.AddTransient<AuthorizationMessageHandler>();
+            builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("PlannerApp.Api"));
             builder.Services.AddMudServices();
+            builder.Services.AddBlazoredLocalStorage();
+
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
+
 
             await builder.Build().RunAsync();
         }
